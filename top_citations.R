fd3a0cd64f1b3dfdc3d7be04adbf305a6bb6c109
¨Jordan <jpoles1@gmail.com>
2016-05-09 00:47:02 -0500

    Shortened citaiton list

--- top_citations.R
+++ top_citations.R
@@ -1,6 +1,6 @@
 source("initial.R")
-top_citations = queryDB("SELECT VIC_LEGAL_DESCRIPTION as citation, COUNT(*) as ct FROM ticket_data GROUP BY citation ORDER BY CT DESC LIMIT 30")
+top_citations = queryDB("SELECT VIC_LEGAL_DESCRIPTION as citation, COUNT(*) as ct FROM ticket_data GROUP BY citation ORDER BY CT DESC LIMIT 20")
 ggplot(top_citations, aes(x=reorder(citation, ct), y=ct))+
   geom_bar(stat="identity")+coord_flip()+
   xlab("Reason for Citation")+ylab("\nNumber of Tickets\n")+
-  ggtitle("Top 30 Reasons for Citation")
+  ggtitle("Top 20 Reasons for Citation")
